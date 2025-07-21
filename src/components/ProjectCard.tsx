82a216ca7d83871098bb236c28dcac4de8cbab8b
selinuz <eceselinuz2@gmail.com>
2025-07-20 20:42:34 -0700

    added project filtering

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -21,19 +21,24 @@ const ProjectCard: React.FC<Props> = ({
           <a
             href={project.link}
             target="_blank"
+            rel="noopener noreferrer"
             className="project-icon"
             onClick={(e) => e.stopPropagation()}>
             <Image
               src={project.iconType === "github" ? "/github.svg" : "/link.svg"}
-              alt="External Link"
+              alt={
+                project.iconType === "github" ? "GitHub Link" : "External Link"
+              }
               width={20}
               height={20}
             />
           </a>
         )}
       </div>
+
       <p className="duration">{project.duration}</p>
       <p>{project.description}</p>
+
       <div className="skills">
         {project.skills.map((skill) => (
           <div key={skill} className="skill-box">
@@ -41,23 +46,22 @@ const ProjectCard: React.FC<Props> = ({
           </div>
         ))}
       </div>
+
       <p className="click-to-expand">Click to see more...</p>
 
       {expanded && (
         <div className="project-details">
           {project.detailsTitle && (
-            <p className="detail">{project.detailsTitle}</p>
-          )}
-          <ul>
-            {project.details.map((detail, idx) => (
-              <li key={idx}>{detail}</li>
-            ))}
-          </ul>
-          {project.context && (
-            <p className="project-context">
-              <em>{project.context}</em>
+            <p>
+              <strong>{project.detailsTitle}</strong>
             </p>
           )}
+          <ul>
+            {project.details.map((item, i) => (
+              <li key={i}>{item}</li>
+            ))}
+          </ul>
+          {project.context && <p className="detail">{project.context}</p>}
         </div>
       )}
     </div>
